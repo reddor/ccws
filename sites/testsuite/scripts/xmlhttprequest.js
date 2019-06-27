4512307a5a5657a6e04efb52ef20f17762d909ec
Simon Ley <simon.ley@gmail.com>
2019-06-28 00:12:51 +0200

    make testsuite a little less dangerous

--- sites/testsuite/scripts/xmlhttprequest.js
+++ sites/testsuite/scripts/xmlhttprequest.js
@@ -2,6 +2,12 @@ handler.onRequest = function(client) {
 	try {
 		console.log("Got request for " + client.parameter);
 		var r = new XMLHttpRequest();
+		let x = client.parameter.split("/");
+		// lets not be a generic proxy
+		if ((x[1] != "/dummy.txt") || x.length != 2) {
+			client.disconnect();
+			return;
+		}
 	        r.open("GET", client.parameter);
 	        r.onreadystatechange = function(e) {
 			if(r.readyState == 4) {
